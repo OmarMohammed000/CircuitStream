009bb2e0e9816718f4de8ecb3fff81fcf6f6ed34
omar <omarmohammedelsayed00>
2024-05-03 10:28:21 +0300

    changing color schme and adding in articals

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get("/",(req,res)=>{
     res.render("index.ejs");
 })
+app.get("/home",(req,res)=>{
+    res.render("index.ejs");
+})
+
+app.get("/augmented-reality",(req,res)=>{
+    res.render("augmentedReality.ejs")
+})
+app.get("/Quantum-computers",(req,res)=>{
+    res.render("q.ejs")
+})
 
 app.listen(port , ()=>{
     console.log(`Listening on port ${port}`);
